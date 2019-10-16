66616e1f0c886a99595464a668cd07fb2030bf54
Aswathy <aeg@test.com>
2019-10-16 19:15:23 +0530

    Created members model

---
+++ src/app/shared/members.model.ts
@@ -0,0 +1,11 @@
+export class Members{
+//public name:string;
+//public id:number;
+//constructor(name:string,id:number){
+    //this.name=name;
+    //this.id=id;}
+ //Typescript offers a shortcut to replace the above code
+constructor(public name:string,public memberId:number){}
+
+}
+
